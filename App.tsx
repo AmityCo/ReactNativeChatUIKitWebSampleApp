5434b03dfa7dc6aabdd980617223f95c2e73731c
topAmity <112688936+topAmity@users.noreply.github.com>
2024-02-15 12:24:12 +0700

    update chat uikit version

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
-import * as React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Linking } from 'react-native'
 import {
   AmityUiKitProvider,
   AmityUiKitChat,
 } from '@amityco/react-native-chat-ui-kit';
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false)
+  const [primaryColor, setPrimaryColor] = useState<string>()
 
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const darkMode = urlParams.get('darkMode');
+    const primary = urlParams.get('primary');
+    console.log('primary: ', primary);
+    if (darkMode === 'true') {
+      setDarkMode(true)
+    }
+    if(primary){
+      setPrimaryColor(`#${primary}`)
+    }
+  }, [])
+  const myTheme = {
+    primary: primaryColor, // Primary color for main elements
+  };
   return (
     <AmityUiKitProvider
       apiKey="b3babb0b3a89f4341d31dc1a01091edcd70f8de7b23d697f"
@@ -14,6 +31,8 @@ export default function App() {
       userId="John"
       displayName="John"
       apiEndpoint="https://api.sg.amity.co"
+      darkMode={darkMode}
+      theme={myTheme}
     >
       <AmityUiKitChat />
     </AmityUiKitProvider>
