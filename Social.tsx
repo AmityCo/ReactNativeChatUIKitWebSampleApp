853d6c6db6e28630eea95a189c3c512b98067d37
topAmity <112688936+topAmity@users.noreply.github.com>
2024-04-03 15:47:08 +0700

    update

--- Social.tsx
+++ Social.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
-// import { Amplify } from "aws-amplify";
-// import amplifyconfig from "./src/amplifyconfiguration.json";
-// Amplify.configure(amplifyconfig);
+import { Amplify } from "aws-amplify";
+import amplifyconfig from "./src/amplifyconfiguration.json";
+Amplify.configure(amplifyconfig);
 import {
   AmityUiKitProvider,
   AmityUiKitSocial,
