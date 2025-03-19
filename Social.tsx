8bab82e537df3890fc7dc907ef95e85c5d39777e
topAmity <112688936+topAmity@users.noreply.github.com>
2025-03-19 18:34:24 +0700

    remove dup component

--- Social.tsx
+++ Social.tsx
@@ -318,7 +318,7 @@ export default function Social() {
           ) : (
             <AmityUiKitSocial />
           )}
-          <AmityUiKitSocial />
+      
         </div>
       </AmityUiKitProvider>
     )
